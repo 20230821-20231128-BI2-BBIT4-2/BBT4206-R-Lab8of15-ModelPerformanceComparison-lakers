6d6a4defa9a98b0cb0d09a3ad54a71c5c958598f
Trevor Augustine <trevor.okinda@strathmore.edu>
2023-11-05 15:28:18 +0300

    Load dataset

---
+++ Lab8-Submission-ModelPerformanceComparison.R
@@ -0,0 +1,9 @@
+# Load necessary libraries
+library(caret)
+library(e1071)
+library(randomForest)
+library(kernlab)
+library(rpart)
+
+# Load the Iris dataset (this dataset is included in R)
+data(iris)
